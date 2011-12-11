83e696533cc6f38fc4e2daca76df614df4a8ed6e
John P. Reardon <jp@jpreardon.com>
2011-12-11 16:33:04 -0500

    Removing VarSpeedServo library due to incompatibility, commiting some work on slow movement

--- fiveServo/fiveServo.ino
+++ fiveServo/fiveServo.ino
@@ -1,11 +1,14 @@
 // 5 Servos Controlled by a total amature
-// 2011-11-25
+// Created: 2011-11-25
 // JP Reardon http://jpreardon.com/
+// Modified 2011-12-11: Testing out slow servo functions
 
-// #include <Servo.h>
-#include <VarSpeedServo.h>
-// Servo array
-VarSpeedServo servos[5];
+
+#include <Servo.h>
+//include <VarSpeedServo.h>
+//Servo array
+//VarSpeedServo servos[5];
+Servo servos[5];
 // The number of the pushbutton pin
 const int buttonPin = 8;     
 // Declare and set the run variable
@@ -47,52 +50,50 @@ void loop()
   
   // Check the run state, do stuff it it is true!!!
   if(run == true){
-    zeroAll();
-    delay(1000);
-    // oneAtATime(); 
-    moveAll(89);
-    delay(1000);
-    moveAll(179);
-    delay(1000);
-    moveAll(89);
-    delay(1000);
+    int speed = 15;
+    slowMoveAll(speed, 80, 80, 80, 80, 80);
+    delay(2000);
+    // run through 5 random poses
+    for(int i = 0; i < 5; i++){
+      slowMoveAll(speed, random(0,179), random(0,179), random(0,179), random(0,179), random(0,179));
+      delay(2000);
+    }
   }
 }
 
-// Moves all servos to zero degrees
-void zeroAll(){
-  for(int i = 0; i < 5; i++){
-      servos[i].write(0);
-    }
-}
 
-void onAtATime(){
-    for(int i = 0; i < 5; i++){
-      servos[i].write(0);
-      delay(500);
-    }
-    for(int i = 0; i < 5; i++){
-      servos[i].write(89);
-      delay(500);
-    }
-    for(int i = 0; i < 5; i++){
-      servos[i].write(179);
-      delay(500);
-    }
-    for(int i = 0; i < 5; i++){
-      servos[i].write(89);
-      delay(500);
-    }
-}
-
-void moveAll(int pos){
+// Moves all servos to the desired position at the desired speed, i really wish i could pass an array here, but i can't, a pointer might me good here
+void slowMoveAll(int delayTime, int pos1, int pos2, int pos3, int pos4, int pos5){
+  int finalPosition[5] = {pos1, pos2, pos3, pos4, pos5};
+  
+  int servoPos[] = {0, 0, 0, 0, 0};
+  
+  //get the servo positions
   for(int i = 0; i < 5; i++){
-    servos[i].write(pos);
+    servoPos[i] = servos[i].read();
   }
-}
-
-void moveOne(int servoNo, int pos){
-  servos[servoNo].write(pos);
+  
+  //If we have any servos that need to move, move, then wait a bit
+  for(int j = 0; j < 179; j++){
+  
+    //Move the servos towards desired position, one at a time
+    //This is a hack, it should be a while loop and we should be checking if we actually need to move anything
+    //but I just want to get something working here, cleanup later (famous last words)
+    for(int i = 0; i < 5; i++){
+      if (servoPos[i] != finalPosition[i]) {
+        if (servoPos[i] > finalPosition[i]) {
+          servoPos[i] = servoPos[i] - 1;
+          servos[i].write(servoPos[i]);
+        }
+        else
+        {
+          servoPos[i] = servoPos[i] + 1;
+          servos[i].write(servoPos[i]);
+        } 
+      }
+    }
+    delay(delayTime);
+  }
 }
 
 
