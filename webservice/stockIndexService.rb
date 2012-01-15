acb4939135297688812067c363fa05a953909623
John P. Reardon <jp@jpreardon.com>
2012-01-15 11:47:03 -0500

    Channged delimiter from * to brackets as not to be confused with the *OPEN* and *CLOS* return values on the WiFly board

--- webservice/stockIndexService.rb
+++ webservice/stockIndexService.rb
@@ -28,7 +28,7 @@ get '/data' do
   
   # Return the values
   # TODO: Instead of returning a line of text, we should probably return some XML or something, this seems pretty hacky as it is
-  "*#{outputArray[0].to_i},#{outputArray[1].to_i},#{outputArray[2].to_i},#{outputArray[3].to_i},#{outputArray[4].to_i}*"
+  "[#{outputArray[0].to_i},#{outputArray[1].to_i},#{outputArray[2].to_i},#{outputArray[3].to_i},#{outputArray[4].to_i}]"
   
   
   # TODO: Log the API hit. Store the time and the device ID.
